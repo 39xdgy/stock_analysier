96a93dbaaf27b7fa71a9a9d4552bd016c4c226f0
39xdgy <39xdgy@gmail.com>
2021-10-18 22:05:07 -0400

    clean

--- Test/update_stock_ticker_test.py
+++ Test/update_stock_ticker_test.py
@@ -26,66 +26,14 @@ for ficheiro in ["nasdaqlisted.txt", "otherlisted.txt"]:
             ticker = line.split("|")[0]
             if 'File Creation Time' not in ticker and 'Symbol' not in ticker and "$" not in ticker and "." not in ticker and ticker not in test_tickers:
                 test_tickers.append(ticker)
-            '''
-            if re.search("Common Stock", line):
-                ticker = line.split("|")[0]
-                # Append tickers to file tickers.txt
-                open("../Data/tickers.txt","a+").write(ticker + "\n")
-            '''
+
 
 
 print(len(test_tickers))
-'''
-tickers = ''
-with open('../Data/tickers.txt', 'r') as f:
-    tickers = f.read()
 
-tickers = tickers.split('\n')[:-1]
-
-
-tickers = [each_ticker for each_ticker in tickers if ('.' not in each_ticker)]
-print(len(tickers))
-nasdaq_list = []
-
-lines = []
-with open('../Data/nasdaqlisted.txt', 'r') as f:
-    lines = f.readlines()
-    #print(len(lines))
-
-for each_line in lines:
-    ticker = each_line.split('|')[0]
-    if 'File Creation Time' not in ticker and 'Symbol' not in ticker and "$" not in ticker and "." not in ticker:
-        nasdaq_list.append(ticker)
-        if ticker not in tickers:
-            tickers.append(ticker)
-
-print(len(tickers))
-
-lines = []
-other_list = []
-
-with open('../Data/otherlisted.txt', 'r') as f:
-    lines = f.readlines()
-    #print(len(lines))
-
-for each_line in lines:
-    ticker = each_line.split('|')[0]
-    if 'File Creation Time' not in ticker and 'Symbol' not in ticker and "$" not in ticker and "." not in ticker:
-        other_list.append(ticker)
-        if ticker not in tickers:
-            tickers.append(ticker)
-
-
-print(len(tickers))
-
-#all_tickers_str = ' '.join(tickers)
+all_tickers_str = " ".join(test_tickers)
 
 test_read_all = yf.download(all_tickers_str)
 print(test_read_all)
 
 
-
-for ticker in tickers:
-    if ticker not in test_tickers:
-        print(ticker)
-'''
