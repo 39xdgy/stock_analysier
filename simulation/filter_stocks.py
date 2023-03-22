226aa9ee188c5946b32c56cf701a1ff4d3623c35
Zon Wei <zonwei@zons-mbp.myfiosgateway.com>
2023-03-22 16:45:49 -0400

    add data

---
+++ simulation/filter_stocks.py
@@ -0,0 +1,256 @@
+import datetime as dt
+import json
+from stock_data import stock_data
+
+def filter_sort():
+    with open('../Data/all_stock_1d_output.txt', 'r') as f:
+        stock_info = {}
+        each_stock = {}
+
+        for line in f:
+            info = line.split(" ")
+
+            if "->" in line:
+                each_stock['name'] = info[0]
+                each_stock['final_value'] = float(info[-1][:-1])
+            if "%" in line:
+                each_stock['trade_count'] = float(info[0])
+                each_stock['fail_chance'] = float(info[2][:-1])
+            if "[" in line:
+                each_stock['avg_day'] = float(info[0])
+                each_stock['short_day'] = int(info[-2][1:-1])
+                each_stock['long_day'] = int(info[-1][:-2])
+                stock_info[each_stock['name']] = each_stock
+                each_stock = {}
+
+    sorted_list = sorted(stock_info.values(), key=lambda x: (x['fail_chance'], -x['final_value']))[:10]
+
+    #for stock in sorted_list:
+        #print(f'stock: {stock["name"]}\t count: {stock["trade_count"]}\t fail: {stock["fail_chance"]}\n')
+
+    with open('../Data/top_10_output.json', 'w') as f:
+        json.dump(sorted_list, f)
+    
+def run_filter(start, end, each_stock):
+    stock_info = ['kdjj']   #['macd', 'macds', 'macdh', 'kdjk', 'kdjd', 'kdjj', 'rsi_6', 'rsi_12', 'rsi_14']
+
+    write_info = ""
+    total_outcome = 0
+    fail_list = []
+
+    try:
+
+        #######          change param here         #############
+        # pandas datetime to unix timestamp
+        start = start.value//10**9
+        end = end.value//10**9
+        stock = stock_data(stock_name=each_stock, start = start, end = end)
+        stock.get_stats_info(stock_info)
+
+        #print(stock.get_stock_data())
+        indicator_low = 15
+        indicator_high = 85
+
+        #macd_low = 5
+        #macd_high = 95
+
+        stock.set_buy_flag({"kdjj": indicator_low})
+        stock.set_sell_flag({"kdjj": indicator_high})
+        # stock.set_buy_flag({"macd": macd_low})
+        # stock.set_sell_flag({"macd": macd_high})
+        
+        time_diff = 0
+        time_sum = 0
+        time_tracker = [-1, -1]
+        base_value = 10000
+        stock_num = 0
+        flag = True
+
+        sell_flag = False
+
+        buy_value = 0
+
+        trade_count = 0
+        fail_count = 0
+        value_record = 10000
+        temp_price = 0.0
+
+        for index, row in stock.get_stock_data().iterrows():
+            #print(row['macdh'])
+            diff = row['kdjk'] - row['kdjd']
+            
+            if flag:
+                if row['kdjj'] < indicator_low:
+                    #print(row['kdjj'])
+                    sell_flag = True
+                    trade_count += 0.5
+                    flag = False
+                    time_diff += 1
+
+                    temp_price = row['close']
+                    stock_num = base_value // row['close']
+                    base_value -= stock_num * row['close']
+                    #print(f'{each_stock} buying with {stock_num} of stocks. Orgin value is {base_value}.\n')
+
+            elif sell_flag:
+                if row['kdjj'] > indicator_high:# or (row['close'] - temp_price) / temp_price >= 0.02:
+                    #(row['kdjj'])
+                    #print((row['close'] - temp_price) / temp_price)
+                    trade_count += 0.5
+                    flag = True
+                    sell_flag = False
+
+                    if time_tracker[0] == -1:
+                        time_tracker[0] = time_diff
+                        time_tracker[1] = time_diff
+
+                    if time_diff < time_tracker[0]: time_tracker[0] = time_diff
+                    if time_diff > time_tracker[1]: time_tracker[1] = time_diff
+
+                    time_sum += time_diff
+                    time_diff = 0
+                    base_value += stock_num * row['close']
+                    #print(base_value)
+                    stock_num = 0
+                    if value_record > base_value: fail_count += 1
+                    value_record = base_value
+                    #print(f'{each_stock} selling.value is {base_value}.\n')
+                else:
+                    time_diff += 1
+
+        if stock_num != 0:
+            base_value += stock.get_current_price() * stock_num
+
+        ## within 1 day
+        #if fail_count / trade_count <= 0.25 and trade_count >= 15 and base_value > 10150:
+        ## 1 day interval
+        if fail_count / trade_count <= 0.25 and base_value > 11000:
+            write_info += f'{each_stock} \t 10000 -> {base_value}\n'
+            write_info += f'{trade_count} \t {fail_count / trade_count}% of fail\n'
+            write_info += f'{time_sum / trade_count} \t {str(time_tracker)}\n\n'
+        
+        total_outcome += base_value
+                
+        # print(f'{each_stock} is finished. here is the output!')
+        # print(f'\t Outcome: {base_value}')
+        # print(f'\t Trade number: {trade_count}')
+        # print(f'\t Fail%: {fail_count / trade_count}')
+        # print(f'\t Avg time: {time_sum / trade_count}')
+        #print(time_tracker)
+        
+    except:
+        #print(each_stock)
+        fail_list.append(each_stock)
+    return write_info
+    
+def run_analyze(start, end, each_stock):
+    stock_info = ['kdjj']   #['macd', 'macds', 'macdh', 'kdjk', 'kdjd', 'kdjj', 'rsi_6', 'rsi_12', 'rsi_14']
+
+    write_info = ""
+    total_outcome = 0
+    fail_list = []
+    start_time = dt.datetime.now()
+    try:
+
+        #######          change param here         #############
+        # pandas datetime to unix timestamp
+        start = start.value//10**9
+        end = end.value//10**9
+        stock = stock_data(stock_name=each_stock, start = start, end = end)
+
+        stock.get_stats_info(stock_info)
+
+        #print(stock.get_stock_data())
+        
+        indicator_low = 15
+        indicator_high = 85
+        #macd_low = 5
+        #macd_high = 95
+
+
+        stock.set_buy_flag({"kdjj": indicator_low})
+        stock.set_sell_flag({"kdjj": indicator_high})
+        # stock.set_buy_flag({"macd": macd_low})
+        # stock.set_sell_flag({"macd": macd_high})
+
+        time_diff = 0
+        time_sum = 0
+        time_tracker = [-1, -1]
+        base_value = 10000
+        stock_num = 0
+        flag = True
+
+        sell_flag = False
+
+        buy_value = 0
+
+        trade_count = 0
+        fail_count = 0
+        value_record = 10000
+        temp_price = 0.0
+        for index, row in stock.get_stock_data().iterrows():
+            #print(row['macdh'])
+            diff = row['kdjk'] - row['kdjd']
+            
+            if flag:
+                if row['kdjj'] < indicator_low:
+                    #print(row['kdjj'])
+                    sell_flag = True
+                    trade_count += 0.5
+                    flag = False
+                    time_diff += 1
+
+                    temp_price = row['close']
+                    stock_num = base_value // row['close']
+                    base_value -= stock_num * row['close']
+                    #print(f'{each_stock} buying with {stock_num} of stocks. Orgin value is {base_value}.\n')
+
+            elif sell_flag:
+                if row['kdjj'] > indicator_high:# or (row['close'] - temp_price) / temp_price >= 0.02:
+                    #(row['kdjj'])
+                    #print((row['close'] - temp_price) / temp_price)
+                    trade_count += 0.5
+                    flag = True
+                    sell_flag = False
+
+                    if time_tracker[0] == -1:
+                        time_tracker[0] = time_diff
+                        time_tracker[1] = time_diff
+
+                    if time_diff < time_tracker[0]: time_tracker[0] = time_diff
+                    if time_diff > time_tracker[1]: time_tracker[1] = time_diff
+
+                    time_sum += time_diff
+                    time_diff = 0
+                    base_value += stock_num * row['close']
+                    #print(base_value)
+                    stock_num = 0
+                    if value_record > base_value: fail_count += 1
+                    value_record = base_value
+                    #print(f'{each_stock} selling.value is {base_value}.\n')
+                else:
+                    time_diff += 1
+
+        if stock_num != 0:
+            base_value += stock.get_current_price() * stock_num
+
+        ## within 1 day
+        #if fail_count / trade_count <= 0.25 and trade_count >= 15 and base_value > 10150:
+        ## 1 day interval
+        write_info += f'{each_stock} \t 10000 -> {base_value}\n'
+        write_info += f'{trade_count} \t {fail_count / trade_count}% of fail\n'
+        write_info += f'{time_sum / trade_count} \t {str(time_tracker)}\n\n'
+        
+        total_outcome += base_value
+                
+        # print(f'{each_stock} is finished. here is the output!')
+        # print(f'\t Outcome: {base_value}')
+        # print(f'\t Trade number: {trade_count}')
+        # print(f'\t Fail%: {fail_count / trade_count}')
+        # print(f'\t Avg time: {time_sum / trade_count}')
+        #print(time_tracker)
+        
+    except:
+        #print(each_stock)
+        fail_list.append(each_stock)
+    return {"info": write_info, "outcome": base_value}
---
+++ simulation/run.py
@@ -0,0 +1,81 @@
+import multiprocessing
+from istarmap import *
+from tqdm import tqdm
+import pandas
+import time as T
+import json
+from filter_stocks import run_filter, filter_sort, run_analyze
+from sim_schedule import *
+
+
+def concatenate_string(lock, shared_str, new_str):
+    with lock:  # Acquire a lock to prevent concurrent access
+        shared_str.value += new_str
+    return shared_str.value
+
+def worker(lock, shared_str, start, end, stock):
+    new_str = run_filter(start, end, stock)
+    return concatenate_string(lock, shared_str, new_str)
+
+def main(schedule, n=0):
+    start = T.time()
+    
+    csv_list = pandas.read_csv('../Data/nasdaq.csv')
+    stock_list = csv_list[csv_list.columns[0]]
+    args_iter = []
+    
+    with multiprocessing.Manager() as manager:
+        # Use a Manager to create a shared ctypes string
+        shared_str = manager.Value("ctypes.c_char_p", "")
+        #format the iterable tuples for starmap args
+        lock = manager.Lock()
+        for i in range(len(stock_list)):
+            args_iter.append((lock, shared_str, schedule[n][0], schedule[n][1], stock_list[i]))
+        
+        with multiprocessing.Pool() as pool:
+            # Pass the shared string and the individual strings as arguments to the worker function
+            pool.starmap(worker, args_iter)
+        
+        end = T.time()
+        print(f"Time taken: {end - start} seconds")
+        #print(f"Final string: {shared_str.value}")
+        f = open("../Data/all_stock_1d_output.txt", "w")
+        f.write(shared_str.value)
+        f.close()
+        filter_sort()
+
+def analyze_performance(schedule, n=0):
+    # Load the JSON file
+    with open('../Data/top_10_output.json', 'r') as file:
+        data = json.load(file)
+        out_str = ""
+        outcome_lst = []
+        # Iterate through the JSON objects and print the 'name' field
+        for item in data:
+            stock_name = item['name']
+            stock_anal_info = run_analyze(schedule[n][2], schedule[n][3], stock_name)
+            new_str = stock_anal_info["info"]
+            outcome_lst.append(stock_anal_info["outcome"])
+            out_str += new_str
+        print("average price: " + str(sum(outcome_lst)/len(outcome_lst)))
+        f = open('../Data/top_10_output_prediction.txt', 'w')
+        f.write(out_str)
+        f.close()
+        return sum(outcome_lst)/len(outcome_lst)
+
+if __name__ == "__main__":
+    schedule = rolling_six_mon_two_mon_schedule()
+    schedule.pop()
+
+    five_year_performance = []
+    f = open('../Data/5yr_6mo_2mo_kdjj_15_85.txt', 'w')
+    for i in range(len(schedule)):
+        print(f"Running schedule {i}")
+        main(schedule, i)
+        five_year_performance.append(analyze_performance(schedule, i))
+        f.write(str(five_year_performance[i]) + "\n")
+    print("5yr avg:" + str(sum(five_year_performance)/len(five_year_performance)))
+    print(five_year_performance)
+    f.close()
+
+
